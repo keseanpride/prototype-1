39ae16e3ad2c4b6daaca2c768ee27e1ec2efe727
seanpride <keseanpride12@gmail.com>
2022-01-29 11:50:21 -0600

    Updated Camera Movement
    FollowPlayer script now uses a vector3 variable to offset its position from player vehicle while following it.

--- Prototype-1/Assets/Scripts/FollowPlayer.cs
+++ Prototype-1/Assets/Scripts/FollowPlayer.cs
@@ -6,6 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
     // variables
     public GameObject playerVehicle;
+    private Vector3 cameraOffset = new Vector3(0, 5, -8);
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // main camera's transform is set relative to player vehicle's transform
-        transform.position = playerVehicle.transform.position;
+        // main camera's transform is set relative to player vehicle's transform + its camera offset
+        transform.position = playerVehicle.transform.position + cameraOffset;
     }
 }
