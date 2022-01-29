d7cd1c2afc6f1cf7c14d510a4d1702186cdb1463
seanpride <keseanpride12@gmail.com>
2022-01-29 11:11:23 -0600

    Updated Movement Speed
    PlayerController script now uses a public variable of "playerSpeed" to adjust player vehicle's speed as needed within editor.

--- Prototype-1/Assets/Scripts/PlayerController.cs
+++ Prototype-1/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // variables
+    public float vehicleSpeed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
