0552dc575835f64074a3bc6afcb96302b808016c
seanpride <keseanpride12@gmail.com>
2022-01-29 11:58:11 -0600

    Smoothed Camera Movement
    FollowPlayer script now uses a late update method to compensate for main camera's stuttering effect while following player vehicle.

--- Prototype-1/Assets/Scripts/FollowPlayer.cs
+++ Prototype-1/Assets/Scripts/FollowPlayer.cs
@@ -8,14 +8,12 @@ public class FollowPlayer : MonoBehaviour
     public GameObject playerVehicle;
     private Vector3 cameraOffset = new Vector3(0, 5, -8);
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         // main camera's transform is set relative to player vehicle's transform + its camera offset
         transform.position = playerVehicle.transform.position + cameraOffset;
--- Prototype-1/Assets/Scripts/PlayerController.cs
+++ Prototype-1/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,11 @@ public class PlayerController : MonoBehaviour
     // variables
     public float vehicleSpeed = 10.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         // player vehicle forward movement
